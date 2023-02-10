37ab7335f82d671a3a72e52cb8f61d6bed8a0c84
faseeha.cp <faseeha.cp@teamta.net>
2023-02-10 12:06:55 +0530

    updated

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -6,7 +6,8 @@ const Product = (props) => {
         {props.products.length > 0 ? props.products.map((product) => {
             return (
                 <div className='product-card'>
-                    <h3 title={product.title}>{product.title.slice(0, 18) + ".."}</h3>
+                    <h3 className='product-title' title={product.title}>{product.title.slice(0, 18) + ".."}</h3>
+                    <h3 className='product-title-mobile' title={product.title}>{product.title}</h3>
                     <p>Price <span>$ {product.price}</span></p>
                     <img src={product.image} alt="Product Image" />
                 </div>
